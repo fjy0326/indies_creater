74c52e7ac69fc30c9b830baa5de1509d891f1559
fjy0326 <bungakukai2010@gmail.com>
2024-11-03 23:06:41 +0000

    20241104dbcreate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_11_03_103214) do
+ActiveRecord::Schema.define(version: 2024_11_03_225131) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "genre"
+    t.text "comment_text"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "introduction"
+    t.text "body"
+    t.text "link"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
