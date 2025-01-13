9076c1e44c01838783944435aecf389248e08835
fjy0326 <bungakukai2010@gmail.com>
2025-01-13 09:52:13 +0000

    update

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -4,7 +4,7 @@ class Public::SessionsController < Devise::SessionsController
   def after_sign_in_path_for(resource)
     about_path
   end
-
+  
   def after_sign_out_path_for(resource)
     about_path
   end
